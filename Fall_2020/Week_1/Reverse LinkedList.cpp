ad5467e12b8cee5f24f3b128a91d4f354a0da84f
Diego Prado Chapa <diegochapa01@live.com>
2020-09-03 17:02:16 -0500

    Fixed some issues

--- Fall_2020/Week_1/Reverse LinkedList.cpp
+++ Fall_2020/Week_1/Reverse LinkedList.cpp
@@ -1,12 +1,17 @@
 /*
 Problem: https://leetcode.com/problems/reverse-linked-list/
 
-Aproach: Reverse a singly linked list.
+Aproach: Reverse a singly linked list by getting 3 variables, next, current and prev
+in te cycle you are moving the pointers, current become next, prev become curr and next is 
+one pointer forward of curr. Also, next pointer of curr become prev, to invert te directions 
+of the linked list. And you retrun prev, becosuse in the last position curr is null.
 
 Time complexity: O(n)
 Space complexity: O(n)
 
 */
+
+
 #include <iostream>
 using namespace std;
  struct ListNode {
@@ -17,7 +22,26 @@ using namespace std;
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
 
+void printLinkedList(ListNode*head){
+    while(head){
+        cout << head->val << "->";
+        head = head->next;
+        if (!head){
+            cout << "NULL";
+        }
+    }
+    cout << "\n";
 
+}
+
+void deleteList(ListNode* head) {
+    while (head != nullptr) {
+        auto temp = head->next;
+        delete head;
+        head = temp;
+    }
+       
+}
  ListNode* reverseList(ListNode* head) {
         ListNode* prev = NULL;
         while(head){
@@ -26,13 +50,7 @@ using namespace std;
             prev = head;
             head = next;
         }
-        cout << "Reverse Linked list: ";
-        cout << prev->val << "->";
-        cout << prev->next->val <<"->";
-        cout << prev->next->next->val << "->";
-        cout << prev->next->next->next->val << "->";
-        cout << prev->next->next->next->next->val << "->";
-        cout << prev->next->next->next->next->next;
+        
         return prev;
 
     }
@@ -43,7 +61,15 @@ int main()
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
     head->next->next->next->next = new ListNode(5);
-    cout << "Linked list: 1->2->3->4->5->NULL"<< "\n";
-    reverseList(head);
+
+    cout << "Linked list: ";
+    printLinkedList(head);
+
+    head = reverseList(head);
+
+    cout<< "Reverse Linked List: ";
+    printLinkedList(head);
+
+    deleteList(head);
     return 0;
 }
